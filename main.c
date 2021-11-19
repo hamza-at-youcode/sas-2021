d416283b60e200c6e9fdd97920bc0a2610cbcbd6
hamza-at-youcode <hamzayocude@gmail.com>
2021-11-19 23:31:13 +0100

    - Create display accounts function  - Test functions  - Fixe createAccount function

--- main.c
+++ main.c
@@ -10,12 +10,16 @@ typedef struct account{
 
 void createAccount(Account *a){
     printf("\nEntrer le CIN: ");
+    fflush(stdin);
     gets(a->cin);
     printf("\nEntrer le Nom: ");
+    fflush(stdin);
     gets(a->lname);
     printf("\nEntrer le Prenom: ");
+    fflush(stdin);
     gets(a->fname);
     printf("\nEntrer le Montant: ");
+    fflush(stdin);
     scanf("%f",&a->amt);
 }
 
@@ -27,11 +31,23 @@ void displayAccount(Account a){
     printf("----------------------------\n");
 }
 
+void _displayAccounts(Account a[],int len){
+    for(int i=0;i<len;i++) displayAccount(a[i]);
+}
+
+void _createAccounts(Account *a,int nbrOfAc){
+    for(int i=0;i<nbrOfAc;i++){
+        printf("\n\n***** Compte: [%d/%d] *****\n",i,nbrOfAc);
+        createAccount(&a[i]);}
+}
+
 int main(){
     Account *ac;
-    ac = (Account*)malloc(sizeof(Account));
-    createAccount(ac);
-    displayAccount(*ac);
+    int nbrAc;
+    printf("\nDonner le nombre des comptes a ajouter: ");scanf("%d",&nbrAc);
+    ac = (Account*)malloc(nbrAc*sizeof(Account));
+    _createAccounts(ac,nbrAc);
+    _displayAccounts(ac,nbrAc);
 
 
     return 0;
